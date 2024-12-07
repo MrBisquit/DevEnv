677c76031bca1a583636b85a9706ba008ca49326
William <william@wtdawson.info>
2024-12-07 12:24:58 +0000

    Added banners

--- Engines/WindowsEngine/Program.cs
+++ Engines/WindowsEngine/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.InteropServices;
+﻿using System.IO.Pipes;
+using System.Runtime.InteropServices;
 
 namespace WindowsEngine
 {
@@ -13,6 +14,20 @@ namespace WindowsEngine
                 Console.WriteLine("Platform is not Windows, stopping...");
                 Environment.Exit(-1);
                 return;
+            }
+
+            Console.WriteLine("Starting named pipe instance...");
+            var server = new NamedPipeServerStream("DevEnv_Engines_WindowEngine_Pipe");
+            Console.WriteLine("Awaiting connection on pipe \"DevEnv_Engines_WindowEngine_Pipe\"");
+            server.WaitForConnection();
+            StreamReader reader = new StreamReader(server);
+            StreamWriter writer = new StreamWriter(server);
+            while (true)
+            {
+                var line = reader.ReadLine();
+                Console.WriteLine("Received: " + line);
+                writer.WriteLine(String.Join("", line.Reverse()));
+                writer.Flush();
             }
         }
     }
