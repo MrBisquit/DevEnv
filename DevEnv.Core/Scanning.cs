f1e82123227f0e1681562bf48acc2023a1ac603c
William <william@wtdawson.info>
2024-12-10 22:23:32 +0000

    Added the other types

---
+++ DevEnv.Core/Scanning.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevEnv.Core
+{
+    public static class Scanning
+    {
+        public class ScanningData
+        {
+
+        }
+
+        public static ScanningData RequestScanningData()
+        {
+            return new ScanningData(); // Temporary
+        }
+    }
+}
